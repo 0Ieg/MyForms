03720cf95ca7db518b0e86bc11bdd69b51b2c090
0Ieg <obognov@gmail.com>
2023-06-01 22:41:06 +0300

    Начало создания выпадающего меню gitlab

--- src/UI/commons/svgStorage.tsx
+++ src/UI/commons/svgStorage.tsx
@@ -202,3 +202,11 @@ export const InfoSVG:FC = ()=>{
     </SVGStyled>
   )
 }
+//Menus
+export const DropdownSVG:FC = ()=>{
+  return(
+    <SVGStyled>
+      <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
+    </SVGStyled>
+  )
+}
--- src/UI/menus/gitlab/gitlab.tsx
+++ src/UI/menus/gitlab/gitlab.tsx
@@ -2,15 +2,45 @@ import { FC } from 'react'
 import { useSelector } from 'react-redux';
 import styled from "styled-components";
 import { StateType } from '../../../BLL/store';
+import { DropdownSVG } from '../../commons/svgStorage';
 
 const GitlabMenuStyled = styled.div`
-  
+background-color: #292961;
+.dropdown__current{
+  cursor: pointer;
+  height: 40px;
+  padding: 10px;
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+span{
+  color: #d1d1f0;
+}
+svg{
+  stroke: transparent;
+  fill: #d1d1f0;
+  width: 20px;
+  }
 `
 const GitlabMenu:FC = ()=>{
   const isEnglish = useSelector((state:StateType)=>state.theme.language=='English')
   return(
     <GitlabMenuStyled>
-      GitlabMenu
+      <button className='dropdown__current'>
+        <span>About GitLab</span>
+        <DropdownSVG/>
+      </button>
+      <ul className='dropdown__list'>
+        <li className='dropdown__item'>GitLab: the DevOps platform</li>
+        <li className='dropdown__item'>Explore GitLab</li>
+        <li className='dropdown__item'>Install GitLab</li>
+        <li className='dropdown__item'>How GitLab compares</li>
+        <li className='dropdown__item'>Get started</li>
+        <li className='dropdown__item'>GitLab docs</li>
+        <li className='dropdown__item'>GitLab Learn</li>
+      </ul>
     </GitlabMenuStyled>
   )
 }
