18067525edb36322a62eb57abddc3554d2238496
0Ieg <obognov@gmail.com>
2023-06-08 23:03:11 +0300

    Gitlab меню, доработка

--- src/UI/commons/svgStorage.tsx
+++ src/UI/commons/svgStorage.tsx
@@ -210,3 +210,10 @@ export const DropdownSVG:FC = ()=>{
     </SVGStyled>
   )
 }
+export const BurgerSVG:FC = ()=>{
+  return(
+    <SVGStyled>
+      <path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+    </SVGStyled>
+  )
+}
--- src/UI/menus/gitlab/headerRight.tsx
+++ src/UI/menus/gitlab/headerRight.tsx
@@ -1,15 +1,47 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { BurgerSVG } from '../../commons/svgStorage'
 
 const HeaderRightStyled = styled.div`
 justify-self: end;
+.burger{
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+  background-color: transparent;
+  border-radius: 4px;
+  :hover{
+    background-color: #6666c4;
+  }
+  svg{
+    width: 20px;
+    fill: #d1d1f0;
+  }
+}
+.signUp, .signIn{
+  display: inline-block;
+  border-radius: 4px;
+  height: 32px;
+  padding: 4px 6px;
+  border: 2px solid transparent;
+  font-weight: 600;
+  font-size: 14px;
+}
+.signUp{
+  background-color: #d1d1f0;
+  :hover{
+    
+  }
+}
 `
 export const HeaderRight:FC = ()=>{
   return(
     <HeaderRightStyled>
       <div className="support"></div>
-      <div className="burger"></div>
+      <button className="burger">
+        <BurgerSVG/>
+      </button>
       <Link to={'/signup'} className="signUp">Register</Link>
       <Link to={'/signin'} className="signIn">Sign in</Link>
     </HeaderRightStyled>
