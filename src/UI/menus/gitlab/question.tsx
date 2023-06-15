d41731254d66263a6747da4d4a25ff64457588f6
0Ieg <obognov@gmail.com>
2023-06-15 22:55:39 +0300

    Создание очередного бургера

--- src/UI/menus/gitlab/question.tsx
+++ src/UI/menus/gitlab/question.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import styled from 'styled-components'
 import { DotSVG, DropdownSVG, QuestionSVG } from '../../commons/svgStorage'
 const QuestionStyled = styled.div`
@@ -16,6 +16,7 @@ background-color: #292961;
   align-items: center;
   justify-content: center;
   grid-gap: 8px;
+  position: relative;
   :hover{
   background-color: #6666c4;
   }
@@ -30,16 +31,51 @@ svg:nth-child(2){
   position: absolute;
   width: 40px;
 }
+.question__list{
+  background-color: #fbfafd;
+  display: grid;
+  border-radius: 4px;
+  padding: 8px 0px;
+  position: absolute;
+  z-index: 3;
+  top: 83px;
+}
+.question__item{
+  height: 32px;
+  width: 238px;
+  padding: 8px 12px;
+  display: flex;
+  cursor: pointer;
+  font-size: 16px;
+  color: #333238;
+  :hover{
+    background-color: #c4c8cadf;
+  }
+}
+.display_none{
+  display: none;
+}
 `
 export const Question:FC = ()=>{
+  const [isSelected, setIsSelected] = useState(false)
+  const clickHandler = ()=>{
+    isSelected?setIsSelected(false):setIsSelected(true)
+  }
   return(
     <QuestionStyled>
-      <button className='question'>
+      <button className='question' onClick={clickHandler} onBlur={clickHandler}>
         <QuestionSVG/>
         <DotSVG/>
         <DropdownSVG/>
       </button>
-      <ul className='question__list'>
+      <ul className={isSelected?'question__list': 'question__list display_none'}>
+        <li className="question__item">What's new</li>
+        <li className="question__item">Help</li>
+        <li className="question__item">Support</li>
+        <li className="question__item">Community forum</li>
+        <li className="question__item">Keyboard shortcuts</li>
+        <li className="question__item">Submit feedback</li>
+        <li className="question__item">Contribute to GitLab</li>
       </ul>
     </QuestionStyled>
   )
