cd09c4e84dc1c3909694dcb3d77601e47d80db4d
0Ieg <obognov@gmail.com>
2023-06-28 22:00:01 +0300

    Работа с блоком меню

--- src/UI/menus/habr/headerLeft.tsx
+++ src/UI/menus/habr/headerLeft.tsx
@@ -1,23 +1,53 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
+import habrPath from '../../../BLL/images/habrPath.png'
 
 const HeaderLeftStyled = styled.div`
-  
+display: grid;
+grid-template-columns: repeat(5, max-content);
+align-items: center;
+.header__question{
+  padding: 6px 8px;
+  border: 1px solid #6f7577;
+  border-radius: 3px;
+  text-transform: uppercase;
+  font-size: 14px;
+  color: white;
+  margin-right: 20px;
+  :hover{
+    border-color: white;
+  }
+}
+.header__path{
+  display: grid;
+  grid-template-columns: repeat(2, max-content);
+  grid-gap: 8px;
+  img{
+    width: 20px;
+    height: 20px;
+  }
+  span{
+    color: white;
+  }
+  :hover{
+    span{
+      color: #548eaa;
+    }
+  }
+}
 `
 export const HeaderLeft:FC = ()=>{
   return(
     <HeaderLeftStyled>
-      HeaderLeft
-      <div className="container">
-        <div className="header__logo"></div>
-        <div className="header__delimiter"></div>
-        <div className="header__burger"></div>
-        <Link to='/questions' className="header__question">Как стать автором</Link>
-        <Link to='/career' className="header__path">
-          <span>Бэкендеры, вам сюда</span>
-        </Link>
-      </div>
+      <div className="header__logo"></div>
+      <div className="header__delimiter"></div>
+      <div className="header__burger"></div>
+      <Link to='/questions' className="header__question">Как стать автором</Link>
+      <Link to='/career' className="header__path">
+        <img src={habrPath}/>
+        <span>Бэкендеры, вам сюда</span>
+      </Link>
     </HeaderLeftStyled>
   )
 }
