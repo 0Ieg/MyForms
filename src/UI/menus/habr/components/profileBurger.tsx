84d6bd37d9427aa584356776d783c1ffdffcec5b
0Ieg <obognov@gmail.com>
2023-07-04 23:01:02 +0300

    Работа с блоком меню

--- src/UI/menus/habr/components/profileBurger.tsx
+++ src/UI/menus/habr/components/profileBurger.tsx
@@ -4,14 +4,28 @@ import { HabrCalendarSVG, HabrProfileSVG } from "../../../commons/svgStorage";
 import { Link } from "react-router-dom";
 
 const ProfileBurgerStyled = styled.div`
+position: relative;
+overflow: visible;
 .list{
   position: absolute;
-  top: 120px;
   width: 300px;
+  height: 264px;
   border-radius: 4px;
   padding: 12px 0;
   background-color: white;
-
+  &>*{
+    width: 100%;
+  }
+  .forms{
+  display: flex;
+  .signin{
+    width: 52px;
+    padding: 8px 16px;
+  }
+  .signup{
+    width: 108px;
+  }
+}
 }
 .hide{
   display: none;
@@ -19,16 +33,11 @@ const ProfileBurgerStyled = styled.div`
 button svg{
   width: 24px;
 }
-a{
-  overflow: visible;
-}
-.item{
-  overflow: visible;
-}
+
 button{
   width: 300px;
+  overflow: visible;
 }
-
 `
 export const ProfileBurger:FC = ()=>{
   const [isSelected, setIsSelected] = useState(false)
@@ -39,9 +48,9 @@ export const ProfileBurger:FC = ()=>{
     <ProfileBurgerStyled>
       <button onClick={clickHandler} onBlur={clickHandler}><HabrProfileSVG/></button>
       <div className={isSelected?'list':'list hide'}>
-        <div className="item">
-          <Link to='/signin'>Войти</Link>
-          <Link to='/signup'>Регистрация</Link>
+        <div className="forms">
+          <div className="signin"><Link to={'signin'}>Войти</Link></div>
+          <div className="signup"><Link to={'signup'}>Регистрация</Link></div>
         </div>
         <button className="item">Как стать автором</button>
         <button className="item">Правила сайта</button>
