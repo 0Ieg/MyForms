4e5ef018cfea4d1e2ac56db6ac28b0a5f5e7c35a
0Ieg <obognov@gmail.com>
2023-07-05 23:14:31 +0300

    Изменение структуры выпадающего меню

--- src/UI/menus/habr/navigationRight.tsx
+++ src/UI/menus/habr/navigationRight.tsx
@@ -1,43 +1,73 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import styled from "styled-components";
-import { HabrSearchSVG } from "../../commons/svgStorage";
-import { ProfileBurger } from "./components/profileBurger";
+import { HabrCalendarSVG, HabrProfileSVG, HabrSearchSVG } from "../../commons/svgStorage";
 import { Link } from "react-router-dom";
 
 const NavigationRightStyled = styled.div`
-display: grid;
-grid-template-columns: repeat(2, 24px);
-grid-gap: 12px;
-align-items: center;
-overflow: visible;
-svg{
+.menu-right{
+  display: grid;
+  grid-template-columns: repeat(2, 24px);
+  grid-gap: 12px;
+  align-items: center;
+}
+.search svg{
   fill: #929ca5;
+  width: 18px;
   transition: color 0.1s linear;
   :hover{
     fill: #007aff;
   }
 }
-button, a{
-  width: 24px;
-  height: 24px;
-  background-color: transparent;
-  cursor: pointer;
-  display: grid;
-  align-items: center;
-  justify-items: center;
+.profile{
+  .submenu{
+    position: absolute;
+    z-index: 5;
+    width: 300px;
+    height: 264px;
+    border-radius: 4px;
+    padding: 12px 0;
+    background-color: white;
+  }
+  .hide{
+    display: none;
+  }
 }
-a svg{
-  width: 18px;
-}
-
 `
 export const NavigationRight:FC = ()=>{
+  const [isSelected, setIsSelected] = useState(false)
+  const clickHandler = ()=>{
+    isSelected?setIsSelected(false):setIsSelected(true)
+  }
   return(
     <NavigationRightStyled>
-      <Link to='search'><HabrSearchSVG /></Link>
-      <div className="profile">
-        <ProfileBurger />
-      </div>
+      <ul className='menu-right'>
+        <li className='search'>
+          <Link to='search'>
+            <HabrSearchSVG />
+          </Link>
+        </li>
+        <li className='profile'>
+          <button onClick={clickHandler} onBlur={clickHandler}><HabrProfileSVG /></button>
+          <ul className={isSelected ? 'submenu' : 'submenu hide'}>
+            <li className="forms">
+              <div className="signin"><Link to={'signin'}>Войти</Link></div>
+              <div className="signup"><Link to={'signup'}>Регистрация</Link></div>
+            </li>
+            <li>
+              <button className="item">Как стать автором</button>
+            </li>
+            <li>
+              <button className="item">Правила сайта</button>
+            </li>
+            <li>
+              <button className="item">
+                <HabrCalendarSVG />
+                <span>Язык, лента</span>
+              </button>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </NavigationRightStyled>
   )
 }
