a246fb780c3ddf0febaa79c1d553738c2edf147f
0Ieg <obognov@gmail.com>
2023-08-12 23:07:09 +0300

    Работа с оформлением словаря

--- src/UI/english/dictionary.tsx
+++ src/UI/english/dictionary.tsx
@@ -2,13 +2,50 @@ import { FC } from "react";
 import styled from "styled-components";
 
 const DictionaryStyled = styled.div`
-  
+background-color: #22272e;
+height: 100%;
+display: flex;
+flex-direction: column;
+.header{
+  height: 50px;
+  font-size: 32px;
+  font-weight: 500;
+  letter-spacing: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color:#e7e2e2;
+  margin-bottom: 10px;
+  border-radius: 6px;
+}
+.navigation{
+  height: 50px;
+  border-radius: 6px 6px 0 0;
+  background-color:#e7e2e2;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 2px;
+  display: flex;
+  align-items: center;
+}
+.main{
+  flex-grow: 1;
+  background-color: #fff;
+}
+.footer{
+  height: 50px;
+  border-radius: 0 0 6px 6px;
+  background-color:#e7e2e2;
+}
 `
 
 const Dictionary:FC = ()=>{
   return(
     <DictionaryStyled>
-      DictionaryStyled
+      <header className="header">Dictionary</header>
+      <nav className="navigation">Navigation</nav>
+      <main className="main">Main</main>
+      <footer className="footer">Footer</footer>
     </DictionaryStyled>
   )
 }
--- src/UI/english/numbers.tsx
+++ src/UI/english/numbers.tsx
@@ -2,19 +2,23 @@ import { FC } from "react";
 import styled from "styled-components";
 
 const NumbersStyled = styled.div`
-background-color: #d4d3d3;
+background-color: #22272e;
 height: 100%;
 .header{
+  height: 50px;
+  margin-bottom: 10px;
+  border-radius: 6px;
   font-size: 32px;
   font-weight: 500;
-  padding: 9px;
-  letter-spacing: 5px;
+  letter-spacing: 4px;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #e7e2e2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-
 `
 const Numbers:FC = ()=>{
   return(
